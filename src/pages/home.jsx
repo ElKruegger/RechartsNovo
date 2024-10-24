62984d8d8db02dfb0dc96a4233ce2f54e0815b4f
erick <erick.krug@skysoftware.com.br>
2024-10-24 11:08:59 -0300

    #Security commit, i finnaly add that feature i was talking abt changing charts on click, so i made this and for now i gonna work on C# to push some data for my charts. keep working

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { RenderLineChart } from "../components/charts/LineChart";
 import { RenderBarChart } from "../components/charts/barchart";
 import { RenderAreaChart } from "../components/charts/AreaChart";
@@ -7,22 +8,26 @@ import { MdOutlineSsidChart } from "react-icons/md";
 import { RiBarChartHorizontalFill } from "react-icons/ri";
 
 export default function Home() {
-  return (
-    <div className="flex h-screen w-screen">
-      <Navbar />
-      <div className="flex flex-1 flex-col w-full p-9 items-center">
-        <RenderAreaChart />
-        {/* <RenderLineChart /> */}
-        {/* <RenderBarChart /> */}
-      <div className="flex  items-center w-60 h-32 justify-center bg-zinc-700 rounded-3xl transition duration-500  hover:scale-125">
-      <IoStatsChart className="mx-2 rounded-3xl border-2 border-white-700  hover:scale-120 text-3xl hover:border-blue-500"/>
-      <MdOutlineSsidChart className="mx-2 rounded-3xl border-2 border-white-700 hover:scale-120 text-3xl hover:border-blue-500" />
-      <RiBarChartHorizontalFill className="mx-2 rounded-3xl border-2 border-white-700 hover:scale-120 text-3xl hover:border-blue-500" />
 
-        <div>
+  const [chart, setChart] = useState('area');
+
+  const handleChartChange = (newChart) => {
+    setChart(newChart);
+  };
+
+    return (
+      <div className="flex h-screen w-screen">
+        <Navbar />
+        <div className="flex flex-1 flex-col w-full p-9 items-center">
+        {chart === 'area' ? <RenderAreaChart /> : null}
+          {chart === 'line' ? <RenderLineChart /> : null}
+          {chart === 'bar' ? <RenderBarChart /> : null}
+          <div className="flex  items-center w-60 h-24 justify-center bg-zinc-700 rounded-3xl transition duration-500 ">
+            <IoStatsChart className="mx-2 rounded-3xl border-2 border-white-700  hover:scale-120 text-3xl hover:border-blue-500" onClick={() => handleChartChange('bar')} />
+            <MdOutlineSsidChart className="mx-2 rounded-3xl border-2 border-white-700 hover:scale-120 text-3xl hover:border-blue-500" onClick={() => handleChartChange('area')}/>
+            <RiBarChartHorizontalFill className="mx-2 rounded-3xl border-2 border-white-700 hover:scale-120 text-3xl hover:border-blue-500" onClick={() => handleChartChange('line')}/>
+          </div>
         </div>
       </div>
-      </div>
-    </div>
-  )
-} 
+    )
+  } 
